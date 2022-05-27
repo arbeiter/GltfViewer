3fdf5b4f9b3254cd31e849767c51100d87d95ff0
bloxel <bloxel@gmail.com>
2022-05-27 14:21:01 -0700

    Update gltfscene with libraries

--- src/gltfscene.h
+++ src/gltfscene.h
@@ -1,7 +1,13 @@
 #include <glad/glad.h>
-#define TINYGLTF_IMPLEMENTATION
 #define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
 #define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb_image_write.h>
+
+#define TINYGLTF_IMPLEMENTATION
+#define TINYGLTF_NO_INCLUDE_STB_IMAGE_WRITE
+#define TINYGLTF_NO_INCLUDE_STB_IMAGE
 #include "tiny_gltf.h"
 
 class Scene
