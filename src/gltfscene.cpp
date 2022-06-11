af70f212e11a8cbfab4372753991042b7b225193
bloxel <bloxel@gmail.com>
2022-06-10 19:36:02 -0700

    Include glm

--- src/gltfscene.cpp
+++ src/gltfscene.cpp
@@ -1,4 +1,7 @@
 #include "gltfscene.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <iostream>
 #define BUFFER_OFFSET(i) ((char *)NULL + (i))
 
