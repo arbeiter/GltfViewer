c8cba788a592926227d26242b6da015d10c8810d
arbeiter <arbeiter@github.com>
2022-08-08 12:28:54 -0700

    Add debug callbacks

--- src/main.cpp
+++ src/main.cpp
@@ -178,6 +178,55 @@ int ShowStyleSelector(const char* label, int &model_number)
     return model_number;
 }
 
+static void GLAPIENTRY glDebugCallback(GLenum source, GLenum type, GLuint, GLenum severity, GLsizei,
+                                       const GLchar* message, const void*)
+{
+    // clang-format off
+    const char* severityString = "?";
+    switch (severity) {
+        case GL_DEBUG_SEVERITY_HIGH:            severityString = "high";    break;
+        case GL_DEBUG_SEVERITY_MEDIUM:          severityString = "medium";  break;
+        case GL_DEBUG_SEVERITY_LOW:             severityString = "low";     break;
+        case GL_DEBUG_SEVERITY_NOTIFICATION:                                return;
+    }
+
+    const char* sourceString = "?";
+    switch (source) {
+        case GL_DEBUG_SOURCE_API:               sourceString = "API";               break;
+        case GL_DEBUG_SOURCE_WINDOW_SYSTEM:     sourceString = "window system";     break;
+        case GL_DEBUG_SOURCE_SHADER_COMPILER:   sourceString = "shader compiler";   break;
+        case GL_DEBUG_SOURCE_THIRD_PARTY:       sourceString = "third party";       break;
+        case GL_DEBUG_SOURCE_APPLICATION:       sourceString = "app";               break;
+        case GL_DEBUG_SOURCE_OTHER:             sourceString = "other";             break;
+    }
+
+    const char* typeString = "?";
+    switch (type) {
+        case GL_DEBUG_TYPE_ERROR:                   typeString = "error";               break;
+        case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:     typeString = "deprecated behavior"; break;
+        case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:      typeString = "undefined behavior";  break;
+        case GL_DEBUG_TYPE_PORTABILITY:             typeString = "portability";         break;
+        case GL_DEBUG_TYPE_MARKER:                  typeString = "marker";              break;
+        case GL_DEBUG_TYPE_PUSH_GROUP:              typeString = "push group";          break;
+        case GL_DEBUG_TYPE_POP_GROUP:               typeString = "pop group";           break;
+        case GL_DEBUG_TYPE_OTHER:                   typeString = "other";               break;
+    }
+    // clang-format on
+
+    fprintf(stderr, "OpenGL Message.\n Type: %s\nSeverity: %s\nSource: %s\nMessage: %s\n\n", typeString, severityString,
+            sourceString, message);
+
+    if (severity == GL_DEBUG_SEVERITY_HIGH)
+        throw(std::runtime_error("GL Error"));
+}
+
+static void initGLDebug() {
+    glEnable(GL_DEBUG_OUTPUT);
+    glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+    glDebugMessageCallback(glDebugCallback, NULL);
+    glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, GL_TRUE);
+}
+
 int main()
 {
   glfwInit();
@@ -199,6 +248,11 @@ int main()
   }
 
   initImgUi(window);
+  int flags; glGetIntegerv(GL_CONTEXT_FLAGS, &flags);
+  if(flags & GL_CONTEXT_FLAG_DEBUG_BIT ) {
+    std::cout << "Debug flags set" << std::endl;
+    initGLDebug();
+  }
   displayLoop(window);
   glfwTerminate();
   return 0;
